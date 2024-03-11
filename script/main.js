c99ba0556131933914fce93cc016020824c6ed07
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2024-03-11 21:29:55 +0300

    classes

--- script/main.js
+++ script/main.js
@@ -1,151 +1,186 @@
-function Transpoort(wheels, pasanger) {
-    this.wheels = wheels;
-    this.pasanger = pasanger;
-    this.freePlace = 0;
-    this.setFreePlace = (val) => {
-        if (val <= this.pasanger) {
-            this.freePlace = val;
-        }
-    };
-    this.parentMethod = () => this.freePlace + 4;
-}
+class User {
+    // #age;
+    constructor(name, lastName, age){
+        this.name = name;
+        this._lastName = lastName;
+        this.age = age;
+        this.count = 0;
+    }
 
-function Bike(wheels, pasanger, mark, maxSpeed) {
-    Transpoort.apply(this, arguments); //Transpoort.call(this, wheels, pasanger);
-    this.mark = mark;
-    this.maxSpeed = maxSpeed;
-    this.speed = 0;
-    this.setSpeed = (val) => {
-        if (val <= this.maxSpeed) {
-            this.speed = val;
-        }
-    };
+    getName(){
+        return this.name;
+    }
 
-    this.parentMethod = (val) => {
-        return (this.freePlace + 4) * val;
+    setName(name){
+        this.name = name;
+    }
+
+    set lastName (val){
+        this._lastName = val;
+    }
+
+    get lastName (){
+        return this._lastName;
     }
 }
 
-const bike = new Bike(2, 0, "Aist", 25);
-console.log(bike);
 
-function User(id, name, lastName, email) {
-    this.id = id;
-    this.name = name;
-    this.lastName = lastName;
-    this.email = email;
-    let privateTest = 1000;
-    Object.defineProperties(this, {
-        test: {
-            enumerable: true,
-            configurable: false,
-            set: (val) => {
-                if (val.length <= 3) {
-                    test = val;
+const user = new User();
+
+console.log(user);
+
+class Admin extends User {
+    constructor(name, lastName, age, status){
+        super(name, lastName, age);
+        this.status = status;
+    }
+
+    getName(){
+        return 'Имя: ' + super.getName(); //uper.getName() --> User.prototype.getName.call(this);
+    }
+}
+
+const admn = new Admin('Petr', 'Petrov', 44, 1);
+
+console.log(admn.getName());
+
+/**
+ * Note - описание одной заметки
+ * Notes - класс для работы с заметками
+ * NoteUI - класс для определения графического интерфейса
+ */
+
+class Note{
+    constructor(data){ // {title, content}
+        // if (data.title.length > 0) this.data = data;
+        this.data = data;
+    }
+
+    edit(data) { // {title, content}
+        // this.data = data;
+        // Object.assign(this.data, data);
+        this.data = {...this.data, ...data}; // {...{title: 'title1', content: 'content1'}, ...{content: 'edit content1'}} -> {title: 'title1', content: 'edit content1'}
+    }
+}
+
+// const note = new Note({title: 'title1', content: 'content1'});
+
+// [...[1, 2, 3,], ...[4, 5, 6]] -> [1, 2, 3, 4, 5, 6]
+
+class Notes{
+    constructor(){
+        this.notes = []; // [Note, Note, Note, .., Note];
+        // this.id = 0
+    }
+
+    add (data){
+        if (data.title === '' || data.content === '') return;
+        const note = new Note(data);
+        const id = Date.now();
+        note.edit({id});
+        this.notes.push(note);
+    }
+
+    remove(id){
+        this.notes = this.notes.filter(note => note.data.id !== id);
+    }
+
+    edit(id, data){
+        const note = this.notes.find(note => note.data.id === id);
+        note.edit(data);
+    }
+}
+
+const notes = new Notes()
+// notes.add({title: 'title1', content: 'content1'});
+// notes.add({title: 'title2', content: 'content2'});
+// notes.add({title: 'title3', content: 'content3'});
+
+class NoteUI extends Notes{
+    constructor(selector){
+        super();
+        this.container = document.querySelector(selector);
+        this.notesContainer = document.createElement('div');
+        this.init();
+    }
+
+    init(){
+        const form = document.createElement('form');
+        const title = document.createElement('input');
+        title.setAttribute('type', 'text');
+        const content = document.createElement('textarea');
+        const submit = document.createElement('button');
+        submit.setAttribute('type', 'submit');
+        submit.innerHTML = 'Add Note';
+        form.append(title, content, submit);
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const data = {
+                title: title.value,
+                content: content.value,
+            }
+            this.add(data);
+            this.render();
+            title.value = '';
+            content.value = '';
+        });
+
+        this.notesContainer.classList.add('notes');
+        this.container.append(form, this.notesContainer);
+        this.render();
+    }
+
+    render(){
+        if (this.notes.length === 0){
+            this.notesContainer.innerHTML = `<h2 class='title'>Список заметок пуст</h2>`;
+            return;
+        }
+        this.notesContainer.innerHTML = '';
+        this.notes.forEach(note => {
+            let flag = false;
+            const noteItem = document.createElement('div');
+            noteItem.classList.add('item');
+            const title = document.createElement('h3');
+            const content = document.createElement('p');
+            const remove = document.createElement('button');
+            remove.classList.add('remove');
+            const edit = document.createElement('button');
+            edit.classList.add('edit');
+            remove.innerHTML = 'Remove';
+            edit.innerHTML = 'Edit';
+            title.innerHTML = note.data.title;
+            content.innerHTML = note.data.content;
+
+            remove.addEventListener('click', () => {
+                this.remove(note.data.id);
+                this.render();
+            });
+
+            edit.addEventListener('click', () => {
+                if (flag){
+                    edit.innerHTML = 'Edit';
+                    title.contentEditable = false;
+                    content.contentEditable = false;
+                    const data = {
+                        title: title.innerText,
+                        content: content.innerText,
+                    }
+                    this.edit(note.data.id, data);
+                    this.render();
+                    flag = !flag;
                 } else {
-                    throw new Error("Некорректная длинна");
+                    edit.innerHTML = 'Save';
+                    title.contentEditable = true;
+                    content.contentEditable = true;
+                    flag = !flag;
                 }
-            },
-            get: () => test,
-        },
-    });
-    this.getTest = () => privateTest;
-}
+            });
 
-User.prototype.setName = function (name) {
-    this.name = name;
-};
-
-User.prototype.setLastName = function (lastName) {
-    this.lastName = lastName;
-};
-
-User.prototype.setEmail = function (email) {
-    this.email = email;
-};
-
-User.prototype.testMethod = function() {
-    return this.id.toUpperCase();
-}
-
-User.prototype.toString = function () {
-    return `Nmae: ${this.name}, LastName: ${this.lastName}, Email: ${this.email}, Test: ${this.test}`;
-}
-
-function Admin(id, name, lastName, email) {
-    //status = 0 - полный доступ, status = 3 - ограниченный доступ
-    User.apply(this, arguments);
-    // this.status = status;
-    Object.defineProperties(this, {
-        status: {
-            enumerable: false,
-            configurable: false,
-            set: (val) => (status = val),
-            get: () => status,
-        },
-    });
-
-    this.testMethod = () => {
-        return User.prototype.testMethod.call(this) + ' test';
+            noteItem.append(title, content, remove, edit);
+            this.notesContainer.append(noteItem);
+        });
     }
 }
 
-Admin.prototype = Object.create(User.prototype);
-
-Admin.prototype.testMethod2 = function (num) {
-    return User.prototype.testMethod.call(this).repeat(num); 
-}
-
-Admin.prototype.toString = function () {
-    return `${User.prototype.toString.call(this)}, Status: ${this.status}`;
-}
-
-function AutorizateUser(id, name, lastName, email, status) {
-    User.apply(this, arguments);
-    this.status = status;
-}
-
-const user = new User("324g5jk34", "Alex", "Petrov", "apetr@test.com");
-try {
-    user.test = "qwe";
-} catch (error) {
-    console.log(error.message);
-} finally {
-    console.log("Около бесполезный блок");
-}
-const admin_1 = new Admin("324g5jk34", "Alex", "Petrov", "apetr@test.com");
-console.log(admin_1);
-admin_1.setEmail("apertov@test2.com");
-console.log(admin_1);
-admin_1.status = 3;
-
-function add(a, b) {
-    if (typeof a === "number" && typeof b === "number") return a + b;
-    else if (typeof a === 'number') return b.repeat(a);
-    else if (typeof b === 'number') return a.repeat(b);
-    else return a + ' ' + b;
-}
-
-function Html(){
-    /**
-     * @param {string} tagName 
-     * @returns {HTMLElement}
-     */
-    this.create = (tagName) => {
-        return document.createElement(tagName);
-    };
-    /**
-     * @param {HTMLElement} elem 
-     * @param {string} name 
-     * @param {string?} value 
-     */
-    this.attr = (elem, name, value) => {
-        elem.setAttribute(name, value ? value : '');
-    }
-}
-
-const $ = new Html();
-
-const div = $.create('div');
-$.attr(div, 'disable');
-console.log(div);
+new NoteUI('#root');
