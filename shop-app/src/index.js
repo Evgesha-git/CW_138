2fcd30a130746dcffe34703192c08a9e37017c79
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2024-03-28 21:26:07 +0300

    shop

--- shop-app/src/index.js
+++ shop-app/src/index.js
@@ -1,12 +1,30 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import '../style/main.scss';
+import "../style/main.scss";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 const header = new Header().render();
 const main = new Main().render();
 const footer = new Footer().render();
 
 root.append(header, main, footer);
+
+const test = {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: { rate: 3.9, count: 120 },
+};
+
+const {
+    id,
+    image,
+    rating: { rate, count: c },
+} = test;
+
+console.log(c);
